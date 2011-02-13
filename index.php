6388b523bbbd6fd56388381cf639f2b903b1edbc
benjamin <benjamin@benjamin-ubuntu.(none)>
2011-02-13 23:10:18 +0100

    added doctrine2 autoload and first simple readme

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
 require_once 'autoload.inc.php';
+require_once 'doctrine.inc.php';
+
 $skillList = new SkillList();
 
 $character = Character::restore('be3efa9d33a16f5e6cae53fc1a34b78f');
 $pc = ($skillList->getSkillByName('Computer'));
 $skillCheck = $character->makeSkillCheck($pc,3,10)->getLatestSkillCheck();
 
-var_dump($skillList->getAllSkills());
+var_dump($skillCheck);
 ?>
